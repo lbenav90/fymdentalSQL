11e302b7be2f03c75dc49dae73a8b75a0b5900ca
lbenav90 <105603962+lbenav90@users.noreply.github.com>
2022-11-29 22:31:42 -0300

    Update files

--- vistas.sql
+++ vistas.sql
@@ -1,1 +1,11 @@
 USE fymdental;
+
+-- Historia clínica de un paciente
+
+CREATE OR REPLACE VIEW historia_clinica AS (
+	SELECT t.fecha, t.hora, p.documento, e.descripcion, em.apellido AS 'Odontólogo'
+	FROM evoluciones e
+	JOIN turnos t ON e.id_turno = t.id_turno
+    JOIN empleados em ON em.id_empleado = e.id_empleado
+	JOIN pacientes p ON e.id_paciente = p.id_paciente
+	ORDER BY fecha, hora);
